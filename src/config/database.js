b8f27716de22f6692644242091e1ba5f071cc7b7
Pierre Boschetto <pierre.mota@gam.com.br>
2021-03-15 12:49:35 -0300

    Config postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,19 @@
 
 module.exports = {
     dialect: 'postgres',
+    url: 'postgres://ugtpnrtuhkfcer:66c51cf257f3ca5a773738920a37decbddff2f55ac9799718bf2f91ff4f3e35f@ec2-52-7-115-250.compute-1.amazonaws.com:5432/dcge13l5pun5dv',
     dialectOptions: {
-        ssl: false
+        rejectUnauthorized: false
     },
-    host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
     define: {
       timestamps: true,
       underscored: true,
     },
   };
+

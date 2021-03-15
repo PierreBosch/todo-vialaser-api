22721c03318d4d33482c8b94079fe918673c978a
Pierre Boschetto <pierre.mota@gam.com.br>
2021-03-15 14:15:35 -0300

    Disable TLS verification Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,22 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
 
 module.exports = {
-    dialect: 'postgres',
-    url: 'postgres://pxbvalel:mB7a6gC3UpP7DXei2kz0b7Dt8mYP6EGZ@ziggy.db.elephantsql.com:5432/pxbvalel',
-    dialectOptions: {
-        rejectUnauthorized: false
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    define: {
-      timestamps: true,
-      underscored: true,
-    },
-  };
+  host: process.env.DB_HOST,
+  dialectOptions: {
+    ssl: true,
+    rejectUnauthorized: false,
+  },
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || "postgres",
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
 
